0101dc088dfa03b582d7dbc649bec9ece5464add
JC Casas <jace.casas@gmail.com>
2017-06-14 18:52:11 -0500

    agregado el contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { AppComponent } from './app.component';
 import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 import { HeroComponent } from './hero/hero.component';
 import { SectionComponent } from './section/section.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 export function HttpLoaderFactory(http: Http) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -25,7 +28,10 @@ export function HttpLoaderFactory(http: Http) {
     AppComponent,
     LanguageSelectorComponent,
     HeroComponent,
-    SectionComponent
+    SectionComponent,
+    ProjectComponent,
+    ProjectListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
