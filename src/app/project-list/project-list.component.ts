2806daf57e7b6e305ff61120adee9ee8a69e001a
JC Casas <jace.casas@gmail.com>
2017-06-14 18:50:28 -0500

    implementados componentes para la lista de proyectos

---
+++ src/app/project-list/project-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { SectionComponent } from '../section/section.component';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['../section/section.component.css']
+})
+export class ProjectListComponent extends SectionComponent { }
